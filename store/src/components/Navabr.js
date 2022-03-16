0a88619d62ebe9ce0932d80aab9040ee26f444ff
Masoud <masoudmotahari1380@gmail.com>
2022-03-16 08:14:47 -0700

    some changes

--- store/src/components/Navabr.js
+++ store/src/components/Navabr.js
@@ -8,15 +8,19 @@ class Navabr extends Component {
         <ul className="flex justify-evenly md:flex-col md:h-48 items-center">
           <li>
             <input id="dress" className="hidden" type={"radio"} name="category" />
-            <label className="mx-2 cursor-pointer hover:text-violet-600 transition-all border-b-2 px-2 py-2  md:my-4 md:py-2" htmlFor="dress">dress</label>
+            <label className="mx-2 cursor-pointer hover:text-violet-600 transition-all border-b-2 px-2 py-2  md:my-4 md:py-2" htmlFor="dress">men's clothing</label>
           </li>
           <li>
             <input id="man" className="hidden" type={"radio"} name="category" />
-            <label className="mx-2 cursor-pointer hover:text-violet-600 transition-all border-b-2 px-2 py-2  md:my-4 md:py-2" htmlFor="man">man</label>
+            <label className="mx-2 cursor-pointer hover:text-violet-600 transition-all border-b-2 px-2 py-2  md:my-4 md:py-2" htmlFor="man">jewelery</label>
+          </li>
+          <li>
+            <input id="man" className="hidden" type={"radio"} name="category" />
+            <label className="mx-2 cursor-pointer hover:text-violet-600 transition-all border-b-2 px-2 py-2  md:my-4 md:py-2" htmlFor="man">electronics</label>
           </li>
           <li>
             <input id="woman" className="hidden" type={"radio"} name="category" />
-            <label className="mx-2 cursor-pointer hover:text-violet-600 transition-all border-b-2 px-2 py-2  md:my-4 md:py-2" htmlFor="woman">woman</label>
+            <label className="mx-2 cursor-pointer hover:text-violet-600 transition-all border-b-2 px-2 py-2  md:my-4 md:py-2" htmlFor="woman">women's clothing</label>
           </li>
         </ul>
       </div>
